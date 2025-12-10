23558452d995fdc6f6f21a83ccdbfbb6d89eb9dd
Jbrown48 <jarrettjbrown@gmail.com>
2025-12-10 12:52:14 +0000

    Update app.py

--- app.py
+++ app.py
@@ -152,7 +152,8 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)    high = daily["High"]
+    app.run(host="0.0.0.0", port=5000)
+    high = daily["High"]
     low = daily["Low"]
     close = daily["Close"]
 
